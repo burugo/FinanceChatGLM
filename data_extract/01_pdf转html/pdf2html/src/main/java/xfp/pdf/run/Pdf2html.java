06905938d60e94778e3b4ebe5670c40329a1a9bc
guoyb <dodogod@qq.com>
2023-08-15 14:25:13 +0800

    增加打印进度

--- data_extract/01_pdf转html/pdf2html/src/main/java/xfp/pdf/run/Pdf2html.java
+++ data_extract/01_pdf转html/pdf2html/src/main/java/xfp/pdf/run/Pdf2html.java
@@ -16,17 +16,20 @@ public class Pdf2html {
 
         File file = new File(Path.inputAllPdfPath);
         if (!file.exists()){
-            System.out.println(file.getAbsolutePath());
+            System.out.println('路径不存在：'+file.getAbsolutePath());
             return;
         }
         File[] files = file.listFiles();
-        for (File f : files) {
+        for (int i = 0; i < files.length; i++) {
+            File f = files[i];
             PDDocument pdd = null;
             try {
                 pdd = PDDocument.load(f);
                 ContentPojo contentPojo = PdfParser.parsingUnTaggedPdfWithTableDetection(pdd);
                 MarkPdf.markTitleSep(contentPojo);
                 FileTool.saveHTML(Path.outputAllHtmlPath, contentPojo, f.getAbsolutePath());
+                int progress = (i + 1) * 100 / files.length;
+                System.out.println("Processing file " + (i + 1) + " of " + files.length + " (" + progress + "%): " + f.getName());
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
